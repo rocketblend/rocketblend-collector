f53b4e73e8cdd3c6ec26aa0dae465c8728b918d6
Elliot <elliotlunness@lazercube.com>
2022-12-18 18:21:45 +0000

    Added custom decode hook

--- pkg/cmd/cli/config/config.go
+++ pkg/cmd/cli/config/config.go
@@ -1,26 +1,51 @@
 package config
 
 import (
+	"reflect"
+
 	"github.com/go-playground/validator"
+	"github.com/mitchellh/mapstructure"
 	"github.com/rocketblend/rocketblend/pkg/core/runtime"
 	"github.com/spf13/viper"
 )
 
 type (
 	Collection struct {
-		Name        string             `yaml:"name" validate:"required"`
-		Description string             `yaml:"description"`
-		Args        string             `yaml:"args"`
-		Includes    []string           `yaml:"includes"`
-		Packages    []string           `yaml:"packages"`
-		Platforms   []runtime.Platform `yaml:"platforms" validate:"required"`
+		Name        string             `mapstructure:"name" validate:"required"`
+		Description string             `mapstructure:"description"`
+		Args        string             `mapstructure:"args"`
+		Includes    []string           `mapstructure:"includes"`
+		Packages    []string           `mapstructure:"packages"`
+		Platforms   []runtime.Platform `mapstructure:"platforms" validate:"required"`
 	}
 
 	Config struct {
-		Library     string       `yaml:"library" validate:"required"`
-		Collections []Collection `yaml:"collections" validate:"required"`
+		Library     string       `mapstructure:"library" validate:"required"`
+		Collections []Collection `mapstructure:"collections" validate:"required"`
 	}
 )
+
+func PlatformHookFunc() mapstructure.DecodeHookFuncType {
+	return func(
+		f reflect.Type,
+		t reflect.Type,
+		data interface{},
+	) (interface{}, error) {
+		// Check that the data is string
+		if f.Kind() != reflect.String {
+			return data, nil
+		}
+
+		// Check that the target type is our custom type
+		if t != reflect.TypeOf(runtime.Platform(0)) {
+			return data, nil
+		}
+
+		// Return the parsed value
+		p := runtime.Platform(0)
+		return p.FromString(data.(string)), nil
+	}
+}
 
 func Load() (config *Config, err error) {
 	viper.SetConfigType("yaml")      // Set the config type to YAML
@@ -35,7 +60,7 @@ func Load() (config *Config, err error) {
 		return nil, err
 	}
 
-	err = viper.Unmarshal(&config)
+	err = viper.Unmarshal(&config, viper.DecodeHook(PlatformHookFunc()))
 
 	validate := validator.New()
 	if err := validate.Struct(config); err != nil {
